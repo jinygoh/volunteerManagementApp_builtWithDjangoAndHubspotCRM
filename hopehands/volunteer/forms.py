e72499d14c233079ff861676136ae4ca08fedd4c
Jin Ying Goh <82461793+jinygoh@users.noreply.github.com>
2025-07-28 14:34:31 +0800

    Delete hopehands directory

--- hopehands/volunteer/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Volunteer
-
-class VolunteerForm(forms.ModelForm):
-    class Meta:
-        model = Volunteer
-        fields = '__all__'
--- hopehands/volunteer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.volunteer_signup, name='signup'),
-    path('success/', views.success, name='success'),
-]
